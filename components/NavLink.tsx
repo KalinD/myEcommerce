38629bd9db6ef2092b4ddda8abf3410a08658998
Kalin Doychev <doiychevk@gmail.com>
2023-08-26 12:14:38 +0200

    Updated hover color

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -9,7 +9,7 @@ type NavLinkProp = {
 };
 
 const NavLink = ({ href, children, onClick, ...rest }: NavLinkProp) => {
-  const classes = "hover-underline-animation p-2 text-lg md:text-xl";
+  const classes = "hover-underline-animation p-2 text-lg md:text-xl cursor-poitner";
   if (href) {
     return (
       <li className="list-none">
