58a90f43e170a2833aa315b27045a7bd91132cc9
Kalin Doychev <doiychevk@gmail.com>
2023-11-28 16:12:19 +0100

    login debug

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -10,19 +10,22 @@ interface RequestBody {
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
   const body: RequestBody = await request.body;
-
+  console.log(`Body: ${body}`)
   const user = await prisma.user.findFirst({
     where: {
       OR: [{ username: body.emailOrUsername }, { email: body.emailOrUsername }],
     },
   });
+  console.log(`User: ${user}`)
   if (user && user.password && (await bcrypt.compare(body.password, user.password))) {
     const { password, ...userWithoutPass } = user;
     const accessToken = SignJwtAccessToken(userWithoutPass)
+    console.log(`AccessToken: ${accessToken}`)
     const result = {
       ...userWithoutPass,
       accessToken
     }
+    console.log(`Result: ${result}`)
     response.send(JSON.stringify(result))
     return
   }
